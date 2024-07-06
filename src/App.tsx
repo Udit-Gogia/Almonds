e0d73d8314a0396f4a161649f2cda7981082e11f
Udit-Gogia <gogiaudit17@gmail.com>
2024-07-06 15:02:44 +0530

    Initialized Projects

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { LandingPage } from './Pages/Landing/LandingPage';
+
+function App() {
+
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}
+
+export default App
