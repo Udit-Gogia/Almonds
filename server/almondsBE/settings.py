12af34f34b6976aae4ab98969b308137eb7d3792
Udit-Gogia <gogiaudit17@gmail.com>
2024-10-17 09:53:20 +0530

    fix: CORS origin host fix

--- server/almondsBE/settings.py
+++ server/almondsBE/settings.py
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "https://almonds.vercel.app",
-    "http://localhost:3000", 
+    "http://localhost:5173", 
 ]
 
 
